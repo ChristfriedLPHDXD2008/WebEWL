00316953c471945861446dbb17d5d7a6803a4d48
Even0900 <github@gaspla.net>
2017-02-24 18:52:56 +0100

    Basic styles and layout

--- frontend/blueprint.php
+++ frontend/blueprint.php
@@ -9,32 +9,53 @@
     <title>Eine Welt Laden</title>
     <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="/styles/css/bootstrap.css" />
+	<link rel="stylesheet" href="/styles/css/stylesheet.css" />
 </head>
 <body>
     <div class="wrapper">
-        <nav>
-            Eine Welt Laden
-        </nav>
-        <div class="content">
-            <aside>
-                <ul>
-                    <li>
-                        Punkt 1
-                    </li>
-                    <li>
-                        Punkt 2
-                    </li>
-                    <li>
-                        Punkt 3
-                    </li>
-                </ul>
-            </aside>
-            <main>
-                <section>
-                    Das ist die Startseite.
-                </section>
-            </main>
-        </div>
+		<nav class="container">
+			<div class="row">
+				<div class="navbar navbar-custom">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="/">Eine Welt Laden</a>
+						</div>
+						<div class="collapse navbar-collapse" id="navbar-collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="#">Link</a></li>
+								<!--
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+									<ul class="dropdown-menu dropdown-menu-right">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</li>
+								-->
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</div>
+			</div>
+		</nav>
+		<main class="container">
+			<aside class="col-md-3">
+				aside
+			</aside>
+			<article class="col-sm-9">
+				MAIN CONTENT
+			</article>
+		</main>
     </div>
 </body>
 </html>
