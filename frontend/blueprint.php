3e68e053cc3d6f1c08c25333a755935c6ada052d
Even0900 <github@gaspla.net>
2017-02-24 17:46:21 +0100

    Basic files added

---
+++ frontend/blueprint.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, min-width=400, min-device-width=400, initial-scale=1" />
+    <meta name="description" content="Pax et Bonum. Eine Welt Laden e.V." />
+    <meta name="keywords" content="Eine Welt Laden, Pax et bonum, Bischofswerda, Dresden, Kaffee, Fair Trade" />
+    <meta name="author" content="Medienwerkstatt Bishofswerda" />
+    <title>Eine Welt Laden</title>
+    <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <nav>
+            Eine Welt Laden
+        </nav>
+        <div class="content">
+            <aside>
+                <ul>
+                    <li>
+                        Punkt 1
+                    </li>
+                    <li>
+                        Punkt 2
+                    </li>
+                    <li>
+                        Punkt 3
+                    </li>
+                </ul>
+            </aside>
+            <main>
+                <section>
+                    Das ist die Startseite.
+                </section>
+            </main>
+        </div>
+    </div>
+</body>
+</html>
