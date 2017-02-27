9802e971c8dd26cedfb387acf55a8614681dc86c
Even0900 <github@gaspla.net>
2017-02-27 21:40:33 +0100

    About page content added

--- frontend/views/about.php
+++ frontend/views/about.php
@@ -1,14 +1,12 @@
 <section>
 	<h1>Wir über uns</h1>
-	<hr>
 	<a href="/content/25jahre_size_Y1920.jpg" class="img-popup-trigger">
 		<img src="/content/25jahre_crop_wide.jpg" class="img-wide" id="image-1" />
 	</a>
-	<br>
+	<hr>
 </section>
 <section>
 	<h3>Verein und Weltladen</h3>
-	<hr>
 	<p>
 		Im Dezember 1990 gründeten Mitglieder der Franziskusgemeinschaft Leutwitz zusammen mit Freunden und Bekannten den Verein "Pax et bonum" - Eine Welt Laden e.V.
 	</p>
@@ -21,11 +19,39 @@
 	<p>
 		Aktivitäten rund um Verein und Laden werden von den Mitgliedern ehrenamtlich durchgeführt. Der Verein möchte mit seiner Arbeit einen kleinen Beitrag zu einer gerechteren Verteilung in der Welt leisten.
 	</p>
-	<br>
+	<hr>
 </section>
 <section>
 	<h3>Was ist Fairer Handel?</h3>
 	<p>
-		
+		Anfang der 1970er Jahre ist aus der Kritik an ungerechten Welthandelsstrukturen die "Aktion Dritte-Welt-Handel" entstanden. Die heute ca. 800 deutschen Weltläden verstehen sich als Teil dieser Bewegung.
 	</p>
+	<p>
+		<em>Es wurden folgende Kriterien für den Fairen Handel festgelegt:</em>
+	</p>
+	<ul>
+		<li>Sozial- und Umweltverträglichkeit</li>
+		<li>umfassende Transparenz der Strukturen</li>
+		<li>Mitbestimmung in den Fair-Handels-Organisationen</li>
+		<li>Non-Profit</li>
+		<li>Informations- und Bildungsarbeit</li>
+		<li>Kontinuität der Handelsbeziehungen</li>
+	</ul>
+	<hr>
+</section>
+<section>
+	<h3><i class="glyphicon glyphicon-arrow-right"></i> Was kann ich tun?</h3>
+	<p>
+		Sie als Verbraucher können mit dem Einkaufskorb etwas gegen Mangelernährung, Armut und Ungerechtigkeit im Welthandel tun und leisten außerdem einen Beitrag zum Umweltschutz.
+	</p>
+	<hr>
+</section>
+<section>
+	<a href="/content/2015 Lesung mit Anant Kumar.jpg" class="img-popup-trigger">
+		<img src="/content/LesungMitAnantKumar2015_crop.jpg" class="img-wide" id="image-2" />
+	</a>
+	<blockquote>
+		<p><br>Lesung mit Autor Anant Kumar, 2015</p>
+		<footer>Der deutsche Schriftsteller mit indischen Wurzeln schreibt über Erfahrungen in der Fremde</footer>
+	</blockquote>
 </section>
--- js/views/about.js
+++ js/views/about.js
@@ -11,5 +11,26 @@ $(document).ready(function () {
 				});
 			}
 		}
+	});	$('.img-popup-anim-trigger').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-out', // CSS transition easing function
+			opener: function(openerElement) {
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		},
+		callbacks: {
+			open: function() {
+				$('html').css({
+					marginRight: 0,
+					paddingRight: 0,
+					overflow: "auto"
+				});
+			}
+		}
 	});
 });
