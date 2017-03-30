680d872f71141978540b2846e4d28e8435433a36
Even0900 <github@gaspla.net>
2017-03-30 21:40:43 +0200

    OpenStreetMap update

--- frontend/views/store.php
+++ frontend/views/store.php
@@ -11,7 +11,7 @@
 	<br>Individuelle Bestellungen sind (mit entsprechender Wartezeit) jederzeit möglich.
 </p>
 <br>
-<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=14.176566302776338%2C51.12600895967781%2C14.180106818675997%2C51.127579526923&amp;layer=mapnik&amp;marker=51.1267942499761%2C14.178336560726166" style="border: 1px solid black"></iframe>
+<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=14.176566302776338%2C51.12600895967781%2C14.180106818675997%2C51.127579526923&amp;layer=mapnik&amp;marker=51.12680855846433%2C14.17833924293518" style="border: 1px solid black"></iframe>
 <br>
 <div class="text-right">
 	<small>
@@ -21,7 +21,7 @@
 	</small>
 </div>
 <br>
-<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=14.177456796169283%2C51.126412969215856%2C14.179227054119112%2C51.127198252645236&amp;layer=hot&amp;marker=51.12680519176163%2C14.178341925144196" style="border: 1px solid black"></iframe>
+<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=14.176566302776338%2C51.12600895967781%2C14.180106818675997%2C51.127579526923&amp;layer=hot&amp;marker=51.12680855846433%2C14.17833924293518" style="border: 1px solid black"></iframe>
 <div class="text-right">
 	<small>
 		<a href="https://www.openstreetmap.org/?mlat=51.12681&amp;mlon=14.17834#map=20/51.12681/14.17834&amp;layers=H" target="_blank">
